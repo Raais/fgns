cd780b6c9d6b5a2d46dfbf1f753477969eb3d9d8
Raais <64605172+Raais@users.noreply.github.com>
2022-02-15 21:28:53 +0500

    smol fix

--- core/fexport.cc
+++ core/fexport.cc
@@ -6,14 +6,14 @@ bool FGNS::fexport(FGNS::Block &block, std::string src, std::string dst_ext, int
     {
         src = FGNS::root_get_target_fuzzy(block, src);
     }
-    if (dst_ext == "")
-    {
-        dst_ext = src;
-    }
 
     if(FGNS::exists(block, src, mode))
     {
         FGNS::File &file = *FGNS::get_file_ptr(block, src, mode);
+        if (dst_ext == "")
+        {
+            dst_ext = file.name;
+        }
 
         std::ofstream outfile(dst_ext, std::ios::out | std::ios::binary | std::ios::trunc);
         outfile.write(file.content.c_str(), file.content.size());
